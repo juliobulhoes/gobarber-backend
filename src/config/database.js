13384615707595db093768cfee347c3cea7b96b3
fuskyw <juliobulhoes@yahoo.com.br>
2019-12-28 02:22:26 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // yarn add pg pg-hstore
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
